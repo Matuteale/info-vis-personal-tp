923fc93bc4c0e31799fa24c5e39efed1cf370bc1
matuteale <mbuscagl@itba.edu.ar>
2019-04-10 16:18:16 -0300

    Define processedData structure

--- model/processed_data.go
+++ model/processed_data.go
@@ -1,6 +1,20 @@
 package model
 
 type ProcessedData struct {
-	TimestampMs int64 `json:"timestampMs"`
-	LatitudeE7  int64 `json:"latitudeE7"`
+	OnFootTime        map[int]map[int]int64     `json:"onFootTimeSec"`    // By year and month
+	OnVehicleTime     map[int]map[int]int64     `json:"onVehicleTimeSec"` // By year and month
+	OnFootSegments    map[int]map[int][]Segment `json:"onFootSegments"`
+	OnVehicleSegments map[int]map[int][]Segment `json:"OnVehicleSegments"`
+}
+
+type Segment struct {
+	From SegmentPoint  `json:"onFootSegments"`
+	To   *SegmentPoint `json:"onFootSegments,omitempty"`
+}
+
+type SegmentPoint struct {
+	TimestampMs int64  `json:"timestampMs"`
+	LatitudeE7  int64  `json:"latitudeE7"`
+	LongitudeE7 int64  `json:"longitudeE7"`
+	Altitude    *int64 `json:"altitude,omitempty"`
 }
